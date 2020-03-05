d7f0a544785454c78c520c79bc0edef33f6c054d
Filippo <filippotagliacarne@gmail.com>
2020-03-05 15:12:47 +0100

    removed lambda

--- Cit_par.py
+++ Cit_par.py
@@ -38,14 +38,14 @@ ih     = -2 * pi / 180   # stabiliser angle of incidence [rad]
 
 # Constant values concerning atmosphere and gravity
 
-rho0   = 1.2250          # air density at sea level [kg/m^3] 
-lambda = -0.0065         # temperature gradient in ISA [K/m]
+rho0   = 1.2250          # air density at sea level [kg/m^3]
+lam    = -0.0065         # temperature gradient in ISA [K/m]
 Temp0  = 288.15          # temperature at sea level in ISA [K]
 R      = 287.05          # specific gas constant [m^2/sec^2K]
 g      = 9.81            # [m/sec^2] (gravity constant)
 
-# air density [kg/m^3]  
-rho    = rho0 * power( ((1+(lambda * hp0 / Temp0))), (-((g / (lambda*R)) + 1)))   
+# air density [kg/m^3]
+rho    = rho0 * power( ((1+(lam * hp0 / Temp0))), (-((g / (lam*R)) + 1)))
 W      = m * g            # [N]       (aircraft weight)
 
 # Constant values concerning aircraft inertia
@@ -73,7 +73,7 @@ CD = CD0 + (CLa * alpha0) ** 2 / (pi * A * e) # Drag coefficient [ ]
 
 CX0    = W * sin(th0) / (0.5 * rho * V0 ** 2 * S)
 CXu    = -0.02792
-CXa    = +0.47966		# Positive! (has been erroneously negative since 1993) 
+CXa    = +0.47966		# Positive! (has been erroneously negative since 1993)
 CXadot = +0.08330
 CXq    = -0.28170
 CXde   = -0.03728
@@ -90,7 +90,7 @@ Cmadot = +0.17800
 Cmq    = -8.79415
 
 CYb    = -0.7500
-CYbdot =  0     
+CYbdot =  0
 CYp    = -0.0304
 CYr    = +0.8495
 CYda   = -0.0400
@@ -103,7 +103,7 @@ Clda   = -0.23088
 Cldr   = +0.03440
 
 Cnb    =  +0.1348
-Cnbdot =   0     
+Cnbdot =   0
 Cnp    =  -0.0602
 Cnr    =  -0.2061
 Cnda   =  -0.0120
