007a368330110ed66ce9300fb9af8cd05006f180
Filippo <filippotagliacarne@gmail.com>
2020-03-18 10:16:33 +0100

    Added calc_W and V_e_red

--- inputs.py
+++ inputs.py
@@ -7,14 +7,28 @@ b = 15.911
 c_bar = 2.0569
 AR = b*b/S
 
+#Standard Parameters
+
+W_s = 60500 #N
+g_0 = 9.80665 # m/s^2
+lbs = 0.45359 #kg
+inc = 0.0254 #m
+
 # Atmosphere Parameters
 
-p_0 = 101325 # Pa
-a_layer = -0.0065 # K/m
-T_0 = 288.15 # K
-g_0 = 9.80665 # m/s^2
 R = 287.0
+R_sp = 287.058 #J/kgK
+a_layer = -0.0065 # K/m
+
+p_0 = 101325 # Pa
+T_0 = 288.15 # K
+rho_0 = p_0/(R_sp*T_0)
 gamma = 1.4
+
+w_fuel     = 2640*lbs*g_0
+w_fuel_ref = 4050*lbs*g_0
+
+w_oew = 9165.0*lbs*g_0
 
 # Matrices for the flight data
 # nr, time, ET, altitude, IAS, alpha, FFl, FFr, Fused, TAT
@@ -28,6 +42,16 @@ trim_matrix = np.array([[1, 2239, 0, 6060, 161, 5.3, 0, 2.8, 0, 462, 486, 664, 5
 measurement_matrix = np.array([[1,30, 2000, 5010, 249, 1.7, 798, 813, 360, 12.5],[2,2137, 2000, 5020, 221, 2.4, 633, 682, 412, 10.5],[3,2436, 2000, 5020, 192, 3.6, 561, 579, 447, 8.8],[4,2604, 2000, 5030, 163, 5.4, 463, 484, 478, 7.2],[5,2947, 2000, 5020, 130, 8.7, 443, 467, 532, 6],[6,3200, 2000, 5110, 118, 10.6, 474, 499, 570, 5.2] ])
 
 # Adjustments of the measurement matrices to correct units
+def convert(mat: np.ndarray):
+    mat = np.column_stack((mat[:,0:3],mat[:,3]*0.3048,mat[:,4]*0.514444,mat[:,5:-4],mat[:,-4:-2]*(0.453592/3600),mat[:,-2]*0.453592,mat[:,-1]+273.15))
+    return mat
+
+measurement_matrix = convert(measurement_matrix)
+measurement_matrix_real = convert(measurement_matrix_real)
+trim_matrix = convert(trim_matrix)
+trim_matrix_real = convert(trim_matrix_real)
+
+'''
 for row in measurement_matrix:
     row[3] = row[3] * 0.3048
     row[4] = row[4] * 0.514444
@@ -59,3 +83,4 @@ for row in trim_matrix_real:
     row[10] = row[10] * (0.453592/3600)
     row[11] = row[11] * 0.453592
     row[12] = row[12] + 273.15
+'''
