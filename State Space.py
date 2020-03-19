6b4eff1ce3d76f69ae7f413ceb9c219673538af1
Trolinglolz <54899045+Trolinglolz@users.noreply.github.com>
2020-03-19 14:52:08 +0100

    Update State Space.py

--- State Space.py
+++ State Space.py
@@ -1,7 +1,9 @@
 import numpy as np
 import control.matlab as ml
+import control as c
 import matplotlib.pyplot as plt
 
+print("start")
 
 #Standard atmosphere
 g = 9.81
@@ -10,10 +12,9 @@ g = 9.81
 ####INPUTS, Change before use
 V = 250
 u_0= V
-mass = 17000
+mass = 18200
 Weight = mass*g
 density  = 1.225
-
 
 #Using reference values
 oswald_factor = 0.8
@@ -21,7 +22,6 @@ CD_0 = 0.04
 CL_alpha = 5.084
 alpha_0 = 0
 theta_0 = 0
-
 
 span = 15.911
 cord = 2.0569
@@ -31,14 +31,16 @@ Aspect_ratio = span**2/S
 D_b = span/V
 D_c = cord/V
 
-mu_b = mass/ (density*S*span)
-mu_c  = mass / (density*S*cord)
+mu_b = mass / (density*S*span)
+print(mu_b)
+mu_c = mass / (density*S*cord)
 Kx = np.sqrt(0.019)
 Ky = np.sqrt(1.25*1.114)
 Kz = np.sqrt(0.042)
 Kxz = 0.002
 
 CL = 2*Weight/(density*V**2*S)
+print(CL)
 CD = CD_0 + ((CL_alpha*alpha_0)**2/(np.pi*Aspect_ratio*oswald_factor))
 
 
@@ -122,7 +124,7 @@ C1sym = D_c*np.matrix([[-2*mu_c			,						0,								0,											0],
 						[0,										Cm_alpha_dot		,			0,											-2*mu_c*Ky**2]])
 
 C2sym = np.matrix([[Cx_u,									Cx_alpha,						Cz_0,										Cx_q],
-				   [Cz_u,									Cz_alpha,						-Cx_0,										Cz_q],
+				   [Cz_u,									Cz_alpha,						-Cx_0,										Cz_q+2*mu_c],
 				   [0,										0,								0,											1],
 				   [Cm_u,									Cm_alpha,						0,											Cm_q]])
 
@@ -131,12 +133,12 @@ C3sym = np.matrix([[Cx_delta_e],
 				   [0],
 				   [Cm_delta_e]])
 
-C1asym = D_b* np.matrix([[(Cy_beta_dot-2*mu_b)		,			0,								0,											0],
+C1asym = D_b* np.matrix([[(Cy_beta_dot-2*mu_b),					0,								0,											0],
 						[0,										-0.5,							0,											0],
 						[0,										0,								-4*mu_b*Kx**2,								4*mu_b*Kxz],
 						[Cn_beta_dot,							0,								4*mu_b*Kxz,									-4*mu_b*Kz**2]])
 
-C2asym = np.matrix([[Cy_beta,								CL,								Cy_p,										Cy_r-4*mu_b],
+C2asym = np.matrix([[Cy_beta,								CL,								Cy_p,										Cy_r - 4*mu_b],
 					[0,										0,								1,											0],
 					[Cl_beta,								0,								Cl_p,										Cl_r],
 					[Cn_beta,								0,								Cn_p,										Cn_r]])
@@ -149,8 +151,38 @@ C3asym = np.matrix([[Cy_delta_a,							Cy_delta_r],
 Asym = -np.linalg.inv(C1sym)*C2sym
 Bsym = -np.linalg.inv(C1sym)*C3sym
 
+
 Aasym = -np.linalg.inv(C1asym)*C2asym
+#Aasym[0] = [0,0,0,2/D_b]
+
+
+
+
+print("symmetric: ")
+print("A: ",Asym)
+print("B: ",Bsym)
+print(V/cord)
+
+print("Asymmetric: ")
+print("A: ",Aasym)
 Basym = -np.linalg.inv(C1asym)*C3asym
+print("B: ",Basym)
+
+print(2*V/span)
+
+print("debug: ")
+print("cy_beta_dot = ", Cy_beta_dot)
+print("mu_b = ", mu_b)
+print("Cn_bet_dot = ", Cn_beta_dot)
+print("Kx = ", Kx)
+print("Kz = ", Kz)
+print("Kxz = ", Kxz)
+print("Db = ", D_b)
+
+print("C1-1 = ", np.linalg.inv(C1asym))
+
+print("C2 = ", C2asym)
+
 
 Csym = np.matrix(np.identity(4))
 Casym = np.matrix(np.identity(4))
@@ -170,46 +202,60 @@ systemAsym = ml.ss(Aasym,Basym,Casym,Dasym)
 
 print("Eigenvalues symmetric case: ",np.linalg.eigvals(systemSym.A))
 print("Eigenvalues asymmetric case: ", np.linalg.eigvals(systemAsym.A))
-Tin = np.arange(0,1000,0.1)
-ySym, TSym = ml.impulse(systemSym,X0=[0,alpha_0,theta_0,0],T = Tin)
+Tin = np.arange(0,1000,0.01)
+Uin = np.zeros_like(Tin)
+Uin[0:100] = np.radians(5)
 
+TSym, ySym, xOut = c.forced_response(systemSym,T = Tin,U=Uin,X0=[0,alpha_0,theta_0,0])
+#print(ySym)
+#ySym, TSym = ml.step(systemSym,X0=[0,alpha_0,theta_0,0],T = Tin)
 
-yAsym, TAsym = ml.impulse(systemAsym,X0=[0,0,0,0],T=Tin,input = 0)
-print(TSym)
+ySym[0] = ySym[0]*u_0 + u_0
+ySym[3] = ySym[3]*(u_0/cord)
 
+#yAsym, TAsym = ml.impulse(systemAsym,X0=[0,0,0,0],T=Tin,input = 0)
+UinAsym = np.zeros((100000,2))
+UinAsym[0:50,0] = 0.025#np.radians(5)
+print(UinAsym)
+TAsym, yAsym, xOutAsym = c.forced_response(systemAsym,T = Tin,U=np.transpose(UinAsym),X0=[0,0,0,0])
 
-plt.figure()
-plt.plot(TSym,ySym[:,0])
-plt.grid(True)
-plt.ylabel("u")
-plt.figure()
-plt.plot(TSym,ySym[:,1])
-plt.ylabel("alpha")
-plt.grid(True)
-plt.figure()
-plt.plot(TSym,ySym[:,2])
-plt.ylabel("theta")
-plt.grid(True)
-plt.figure()
-plt.plot(TSym,ySym[:,3])
-plt.ylabel("q")
-plt.grid(True)
+yAsym[2] = yAsym[2] * ((2*u_0)/span)
+yAsym[3] = yAsym[3] * ((2*u_0)/span)
+#print(TSym)
+
 
 #plt.figure()
-#plt.plot(TSym,yAsym[:,0])
+#plt.plot(TSym,ySym[0])
 #plt.grid(True)
-#plt.ylabel("sideslip")
+#plt.ylabel("u")
 #plt.figure()
-#plt.plot(TSym,yAsym[:,1])
-#plt.ylabel("roll angle")
-#plt.grid(True)
-#plt.figure()
-#plt.plot(TSym,yAsym[:,2])
-#plt.ylabel("roll rate")
+#plt.plot(TSym,ySym[1])
+#plt.ylabel("alpha")
 #plt.grid(True)
 #plt.figure()
-#plt.plot(TSym,yAsym[:,3])
-#plt.ylabel("yaw rate")
+#plt.plot(TSym,ySym[2])
+#plt.ylabel("theta")
 #plt.grid(True)
+#plt.figure()
+#plt.plot(TSym,ySym[3])
+#plt.ylabel("q")
+#plt.grid(True)
+
+plt.figure()
+plt.plot(TSym,yAsym[0])
+plt.grid(True)
+plt.ylabel("sideslip")
+plt.figure()
+plt.plot(TSym,yAsym[1])
+plt.ylabel("roll angle")
+plt.grid(True)
+plt.figure()
+plt.plot(TSym,yAsym[2])
+plt.ylabel("roll rate")
+plt.grid(True)
+plt.figure()
+plt.plot(TSym,yAsym[3])
+plt.ylabel("yaw rate")
+plt.grid(True)
 
 plt.show()
