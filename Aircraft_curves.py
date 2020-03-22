dad41df510d5e7019ff33b69c5e723ee2f2afc9a
burrzach <61149333+burrzach@users.noreply.github.com>
2020-03-22 21:59:27 +0100

    fixed a mistake I made

--- Aircraft_curves.py
+++ Aircraft_curves.py
@@ -5,6 +5,30 @@ import inputs
 from tools import interpolate
 from scipy import stats
 
+def almost_equal_perc(a, b, percentage=1, return_value=False):
+    """
+    Tests if two floating point numbers are almost equal by comparing their percentual difference against a
+    set value. Default significance is 1%.
+    :param a, b: Floating point numbers to compare
+    :param percentage = Maximum allowed difference in percentage
+    :raises: error when difference falls outside bounds
+    """
+    assert 100*abs(a-b)/(0.5*(a+b)) <= percentage
+    if return_value:
+        return 100*abs(a-b)/(0.5*(a+b))
+
+def almost_equal_abs(a, b, difference=0.01, return_value=False):
+    """
+    Tests if two floating point numbers are almost equal by comparing their absolute difference
+    :param a, b: Floating point numbers to compare
+    :param difference = Maximum allowed absolute difference
+    :raises: error when difference falls outside bounds
+    """
+    assert abs(a-b) <= difference
+    if return_value:
+        return abs(a-b)
+
+
 def get_Thrust(reality:bool, nominal:bool, trim:bool):
     fname = "Thrust"
     if trim:
@@ -27,7 +51,8 @@ def calc_Tc(measurement_matrix, reality:bool, nominal:bool, trim:bool):
         Tc_array.append(Thrust_matrix[i] / (0.5*inputs.rho_0*measurement_matrix[i][4]**2*inputs.d**2))
     return Tc_array
 
-def calc_W(w_f0: float, meas_mat: np.ndarray, ref = True) -> np.ndarray:
+
+def calc_W(w_f0: float,meas_mat: np.ndarray, ref = True) -> np.ndarray:
 
     path = "cg_data/pass_w_ref.dat" if ref else "cg_data/pass_w.dat"
 
@@ -36,18 +61,20 @@ def calc_W(w_f0: float, meas_mat: np.ndarray, ref = True) -> np.ndarray:
 
     return np.sum(w_pass)+ w_f + inputs.w_oew
 
-#def calc_e(): #old version, use calc_CD_curve
-#    Clalpha = 2*math.pi*inputs.AR/(2+math.sqrt(4+inputs.AR**2))
-#    CLalpha = Clalpha*(inputs.AR/(inputs.AR+2))
-#    e = CLalpha/Clalpha
-#    return e
 
 def calc_M(measurement_matrix):
+    '''
+    Inputs:
+     - measurement_matrix = a matrix of the inputs file
+     Outputs:
+      - An array with the Mach number at each measurement point (= row) of the matrix
+    '''
     M_array = []
     for row in measurement_matrix:
         M = row[4]/math.sqrt(inputs.gamma*inputs.R*row[9])
         M_array.append(M)
     return M_array
+
 
 def V_e_red(meas_matrix: np.ndarray, ref: bool, tilda = True, vtas = False):
     p   = inputs.p_0*(1+inputs.a_layer*meas_matrix[:,3]/inputs.T_0)**(-inputs.g_0/(inputs.R*inputs.a_layer))
@@ -63,16 +90,23 @@ def V_e_red(meas_matrix: np.ndarray, ref: bool, tilda = True, vtas = False):
 
     return V*np.sqrt(inputs.W_s/meas_matrix[:,-1]) if tilda else V
 
+
 def de_red(meas_mat: np.ndarray, c_md: float, Tcs: np.ndarray, Tc: np.ndarray):
     if meas_mat.shape[1] < 13:
         return 0
 
     c_mtc = -0.0064
 
-    return meas_mat[:,6] - (c_mtc/c_md)*(Tcs-Tc)
+    return meas_mat[:, 6] - (c_mtc/c_md)*(Tcs-Tc)
 
 
 def calc_deltaT(measurement_matrix):
+    '''
+    Inputs:
+     - measurement_matrix = a matrix of the inputs file
+     Outputs:
+      - An array with the temperature differential with ISA at each measurement point (= row) of the matrix
+    '''
     deltaT_array = []
     for row in measurement_matrix:
         T_ISA = inputs.T_0 + (row[3]*inputs.a_layer)
@@ -80,11 +114,21 @@ def calc_deltaT(measurement_matrix):
         deltaT_array.append(T_delta)
     return deltaT_array
 
+
 def calc_CL(measurement_matrix, ref):
+    '''
+    Inputs:
+     - measurement_matrix = a matrix of the inputs file
+     - ref = a bool that is true if the reference data is used and false if the flight test data is used
+     Outputs:
+      - An array with the C_L at each measurement point (= row) of the matrix
+      Note:
+      - An option to use rho at altitude and V_t is left in the code, but is put in comments for now
+    '''
     C_L_array = []
     C_L_other = []
-    V_e_array = V_e_red(measurement_matrix, ref, False, False) # array with the equivalent airspeed
-    V_t_array = V_e_red(measurement_matrix, ref, False, True) # array with the true airspeed
+    V_e_array = V_e_red(measurement_matrix, ref, False, False)  # array with the equivalent airspeed
+    V_t_array = V_e_red(measurement_matrix, ref, False, True)  # array with the true airspeed
     counter = 0
     for row in measurement_matrix:
         # nr, time, ET, altitude, IAS, alpha, FFl, FFr, Fused, TAT, W
@@ -98,13 +142,22 @@ def calc_CL(measurement_matrix, ref):
     return C_L_array#, C_L_other
 
 def calc_CD_curve(measurement_matrix, reality:bool):
-    D_array = get_Thrust(reality,False,False)
+    '''
+    Inputs:
+     - measurement_matrix = a matrix of the inputs file
+     - reality = a bool that is true if the flight test data is used and false if the reference data is used
+     - ref = a bool that is true if the reference data is used and false if the flight test data is used
+     Outputs:
+      - The estimated value of 'e'
+      - The estimated value of C_D0
+      - An array with the C_D at each measurement point (= row) of the matrix
+    '''
+    D_array = get_Thrust(reality, False, False)
     CL_array = calc_CL(measurement_matrix, not reality)
-    V_e_array = V_e_red(measurement_matrix, not reality, False, False) # array with the equivalent airspeed
+    V_e_array = V_e_red(measurement_matrix, not reality, False, False)
     CD_array = []
     CL2_array = []
     for i in range(len(measurement_matrix)):
-        #rho = (inputs.p_0*(1+(inputs.a_layer*measurement_matrix[i][3]/inputs.T_0))**(-inputs.g_0/(inputs.a_layer*inputs.R)))/(inputs.R*measurement_matrix[i][9])
         rho = inputs.rho_0
         CD_array.append(D_array[i]/(0.5*rho*V_e_array[i]**2*inputs.S))
         CL2_array.append(CL_array[i]**2)
@@ -115,8 +168,17 @@ def calc_CD_curve(measurement_matrix, reality:bool):
     return e,CD0,CD_array
 
 def drag_polar(measurement_matrix, reality:bool):
-    C_L_array = calc_CL(measurement_matrix,not reality)
-    e, CD0, C_D_array = calc_CD_curve(measurement_matrix,reality)
+    '''
+    Inputs:
+     - measurement_matrix = a matrix of the inputs file
+     - reality = a bool that is true if the flight test data is used and false if the reference data is used
+     Outputs:
+      - A plot of the C_L values on the x-axis an the C_D values on the y-axis (not returned)
+      - Returns an array with the C_L at each measurement point (= row) of the matrix
+      - Returns an array with the C_D at each measurement point (= row) of the matrix
+    '''
+    C_L_array = calc_CL(measurement_matrix, not reality)
+    e, CD0, C_D_array = calc_CD_curve(measurement_matrix, reality, not reality)
     e = 0.8
     CD0 = 0.04
     C_D_calculated = []
@@ -150,6 +212,15 @@ def drag_polar(measurement_matrix, reality:bool):
     return C_L_array, C_D_array
 
 def lift_curve(measurement_matrix, ref):
+    '''
+    Inputs:
+     - measurement_matrix = a matrix of the inputs file
+     - ref = a bool that is true if the reference data is used and false if the flight test data is used
+     Outputs:
+      - A plot of the alpha values on the x-axis an the C_L values on the y-axis (not returned)
+      - Returns an array with the alpha at each measurement point (= row) of the matrix
+      - Returns an array with the C_L at each measurement point (= row) of the matrix
+    '''
     Alpha_array = [row[5] for row in measurement_matrix]
     C_L_array = calc_CL(measurement_matrix, ref)
     plt.plot(Alpha_array, C_L_array)
@@ -159,9 +230,19 @@ def lift_curve(measurement_matrix, ref):
     plt.show()
     return Alpha_array, C_L_array
 
-def drag_curve(measurement_matrix,reality):
+
+def drag_curve(measurement_matrix, reality:bool):
+    '''
+    Inputs:
+     - measurement_matrix = a matrix of the inputs file
+     - reality = a bool that is true if the flight test data is used and false if the reference data is used
+     Outputs:
+      - A plot of the alpha values on the x-axis an the C_D values on the y-axis (not returned)
+      - Returns an array with the alpha at each measurement point (= row) of the matrix
+      - Returns an array with the C_D at each measurement point (= row) of the matrix
+    '''
     Alpha_array = [row[5] for row in measurement_matrix]
-    e, CD0, C_D_array = calc_CD_curve(measurement_matrix,reality)
+    e, CD0, C_D_array = calc_CD_curve(measurement_matrix, reality, not reality)
     plt.plot(Alpha_array, C_D_array)
     plt.title('Drag coefficient curve as a function of the angle of attack')
     plt.xlabel('Angle of attack [deg]')
@@ -169,7 +250,15 @@ def drag_curve(measurement_matrix,reality):
     plt.show()
     return Alpha_array, C_D_array
 
-def elevator_curve(measurement_matrix):
+def elevator_curve_alpha(measurement_matrix):
+    '''
+    Inputs:
+     - measurement_matrix = a matrix of the inputs file
+     Outputs:
+      - A plot of the alpha values on the x-axis an the elevator deflection values on the y-axis (not returned)
+      - Returns an array with the alpha at each measurement point (= row) of the matrix
+      - Returns an array with the elevator deflection at each measurement point (= row) of the matrix
+    '''
     Combined_array = [[row[5],row[6]] for row in measurement_matrix]
     ordered_array = []
     for i in range(len(Combined_array)):
@@ -193,8 +282,8 @@ def elevator_curve(measurement_matrix):
     return Alpha_array, De_array
 
 def red_elevator_curve(trim_mat:np.ndarray, ref: bool, c_md: float):
-    Tcs = np.array(calc_Tc(trim_mat, not ref, True))
-    Tc  = np.array(calc_Tc(trim_mat, not ref, False))
+    Tcs = np.array(calc_Tc(trim_mat, not ref, True, True))
+    Tc  = np.array(calc_Tc(trim_mat, not ref, False, True))
 
     V_e_tilda = V_e_red(trim_mat, ref, tilda=True)
     d_e_star  = de_red(trim_mat, c_md, Tcs, Tc)
@@ -202,18 +291,111 @@ def red_elevator_curve(trim_mat:np.ndarray, ref: bool, c_md: float):
     plt.figure("Reduced Elevator Deflection Curve")
     plt.plot(V_e_tilda, d_e_star)
     plt.xlabel("Reduced Airspeed [m/s]")
-    plt.ylabel("Reduced Elevator Deflection [$^{\cirlce}$]")
+    plt.ylabel("Reduced Elevator Deflection [deg]")
+    plt.gca().invert_yaxis()
     plt.title("Reduced Elevator Deflection Curve")
     plt.savefig("figures/red_el_curve.png")
     plt.show()
 
+def red_force_curve(trim_mat:np.ndarray, ref: bool):
+
+    V_e_tilda = V_e_red(trim_mat, ref, tilda=True)
+    F_e_star  = trim_mat[:,8]*inputs.W_s/trim_mat[:,-1]
+
+    sorted_F = F_e_star[V_e_tilda.argsort(axis=0)]
+    sorted_V = V_e_red(trim_mat, ref, tilda=True)
+    sorted_V.sort()
+
+    plt.figure("Reduced Elevator Deflection Curve")
+    plt.plot(sorted_V,sorted_F)
+    plt.xlabel("Reduced Airspeed [m/s]")
+    plt.ylabel("Reduced Elevator Control Force [N]")
+    plt.gca().invert_yaxis()
+    plt.title("Reduced Elevator Control Force Curve")
+    plt.savefig("figures/red_force_curve.png")
+    plt.show()
+    pass
+
+# Test functions (commented to prevent plots from being spammed when running the file)
+
+# Reference Data
+# print(calc_M(inputs.measurement_matrix))
+# print(calc_deltaT(inputs.measurement_matrix))
+# print(calc_CL(inputs.measurement_matrix, ref = True))
+# print(calc_CD_curve(inputs.measurement_matrix, reality = False, ref = True))
+# print(drag_polar(inputs.measurement_matrix, reality = False))
+# print(lift_curve(inputs.measurement_matrix, ref = True))
+# print(drag_curve(inputs.measurement_matrix, reality = False))
+# print(elevator_curve_alpha(inputs.measurement_matrix))
+
+# Flight Test Data
+# print(calc_M(inputs.measurement_matrix_real))
+# print(calc_deltaT(inputs.measurement_matrix_real))
+# print(calc_CL(inputs.measurement_matrix_real, ref = False))
+# print(calc_CD_curve(inputs.measurement_matrix_real, reality = True, ref = False))
+# print(drag_polar(inputs.measurement_matrix_real, reality = True))
+# print(lift_curve(inputs.measurement_matrix_real, ref = False))
+# print(drag_curve(inputs.measurement_matrix_real, reality = True))
+# print(elevator_curve_alpha(inputs.measurement_matrix_real))
+
+# Unit tests performed if you run this file
 
 
-#elevator_curve(inputs.trim_matrix)
-#print(drag_polar(inputs.measurement_matrix_real, True, False))
-#print(lift_curve(inputs.measurement_matrix, False))
-#print(drag_curve(inputs.measurement_matrix_real, True, False))
-#print(calc_CL(inputs.measurement_matrix_real, False))
-#print(calc_M(inputs.measurement_matrix_real))
-#print(calc_deltaT(inputs.measurement_matrix_real))
-#print(elevator_curve(inputs.measurement_matrix_real))
+if __name__ == '__main__':
+    """
+    calcM Test 1
+    At ISA 0ft values, a list of M values is given V values
+    https://www.engineeringtoolbox.com/specific-heat-ratio-d_602.html
+    """
+    x = np.array([[0,0,0,0,100,0,0,0,0,288.15], [0,0,0,0,200,0,0,0,0,288.15]])
+    y = [100/math.sqrt(1.401*287.057*288.15), 200/math.sqrt(1.401*287.057*288.15)]
+    x2 = np.array([[0,0,0,0,0,0,0,0,0,288.15]])
+    y2 = [0]
+    for i in range(len(y)):
+        almost_equal_perc(calc_M(x)[i], y[i], 0.1, True)
+    for i in range(len(x2)):
+        almost_equal_abs(calc_M(x2)[i], y2[i], 10**(-2), True)
+
+    """
+    calcdeltaT Test 1
+    Difference between T_ISA equation and ISA from online sources should be small
+    https://www.digitaldutch.com/atmoscalc/
+    """
+    x = np.array([[0,0,0,0,0,0,0,0,0,288.15], [0,0,0,1000,0,0,0,0,0,281.650], [0,0,0,10000,0,0,0,0,0,223.150]])
+    y = [0, 0, 0]
+    for i in range(len(x)):
+        almost_equal_abs(calc_deltaT(x)[i], y[i], 10**(-2), True)
+
+    """
+    calcCL Test 1
+    Calculates CL for standard values, 0 altitude is taken to not confound with V_red test
+    """
+    x = np.array([[0,0,0,0,100,0,0,0,0,288.15,1000], [0,0,0,0,50,2,0,0,0,288.15,10000]])
+    y = [1000/(0.5*1.225*100*100*30), 10000/(0.5*1.225*50*50*30)]
+    for i in range(len(x)):
+        almost_equal_perc(calc_CL(x, True)[i], y[i], 0.1, True)
+
+    """
+    calcCD_curve Test 1
+    Calculates CD for standard values, 0 altitude is taken to not confound with V_red test
+    """
+    x = np.array([[0,0,0,0,100,0,0,0,0,288.15,1000], [0,0,0,0,50,2,0,0,0,288.15,10000]])
+    y = [7715.38/(0.5*1.225*100*100*30), 6293.42/(0.5*1.225*50*50*30)]
+    for i in range(len(x)):
+        print(calc_CD_curve(x, True, False), y, almost_equal_perc(calc_CD_curve(x, True, False)[2][i], y[i], 0.1, True))
+
+    """
+    elevator_alpha Test 1
+    Calculates whether the alpha array is ever increasing
+    """
+    x, y = elevator_curve_alpha(inputs.measurement_matrix)
+    for i in range(1, len(x)):
+        assert(x[i]-x[i-1] > 0)
+
+    # Functions that still require unit tests
+    # calcTc
+    # calcW
+    # V_e_red
+    # de_red
+    # drag_polar (@Zach once the least squares solution is fixed)
+    # red_elevator_curve
